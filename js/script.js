4d4dd3cd0c00f70e066f5bebe08e87a84a2f66f2
Eliana-Abate <eliana.abate@outlook.it>
2021-07-19 15:31:57 +0200

    es. 1: added object and cycle for-in for printing in console

--- js/script.js
+++ js/script.js
@@ -2,3 +2,20 @@
 Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 Stampare a schermo (in pagina) attraverso un ciclo for-in tutte le proprietà dell'oggetto.*/
 
+//TODO 1. Creo variabile per descrivere l'oggetto con le sue proprietà
+//TODO 2. Creo ciclo FOR-IN per stampare 
+
+
+//! 1. 
+var student = {
+    firstname: 'John',
+    lastname: 'Doe',
+    age: 20,
+};
+
+
+
+//! 2. 
+for (var key in student) {
+    console.log(key + ': ' + student[key]);
+}
