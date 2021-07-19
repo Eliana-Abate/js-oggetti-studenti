9a1e3a2f860368d711ae18f44e37f877d832de6d
Eliana-Abate <eliana.abate@outlook.it>
2021-07-19 15:14:39 +0200

    boilerplate

---
+++ js/script.js
@@ -0,0 +1,4 @@
+/*ES 1
+Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+Stampare a schermo (in pagina) attraverso un ciclo for-in tutte le proprietà dell'oggetto.*/
+
