99de3a01db10fc99a8e1edcc1cedccefdf5125f1
Eliana-Abate <eliana.abate@outlook.it>
2021-07-20 15:22:24 +0200

    edited cycle FOR at lines 112-121

--- js/script.js
+++ js/script.js
@@ -46,9 +46,8 @@ Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo
 //TODO 3. Creo variabili per chiedere all'utente di inserire le proprietà dei nuovi oggetti studenti
    //! 3.1 - validazione
 //TODO 4. Aggiungo oggetto all'array
-//TODO 5. Rifaccio ciclo FOR per stampare il nuovo oggetto studente
-//TODO 6. Creo ciclo FOR-IN all'interno del nuovo ciclo FOR per stampare la proprietà 'età' del nuovo oggetto studente
-//TODO 7. Ristampo lista aggiornata
+//TODO 5. Rifaccio ciclo FOR per stampare la lista aggiornata con il nuovo oggetto studente
+//TODO 6. Stampo lista aggiornata in Html
 
 //! 1. 
 var studentClass = [
@@ -108,20 +107,20 @@ console.dir(studentClass);
 
 //! 5. 
 var displayNewList = document.getElementById('new-list'); 
-
+var newFullName = '';
 
 for (var i = 0; i < studentClass.length; i++) {
     
-    var newCurrentElement = studentClass[i];
-   
-    //! 6.
-    for (var key in studentClass){
-        var newFullName = '';
-        var ageValue = '';
-        newFullName += '<li>' + newCurrentElement.studentName  + ' ' + newCurrentElement.studentSurname + ', età: ' + newCurrentElement.studentAge + ' anni' +  '</li>';
+    var newCurrentElement = studentClass[i]
+    newFullName += '<li>';
+
+    for(var key in newCurrentElement){
+        newFullName += newCurrentElement[key]  +  ' ';
     }
+    newFullName += '</li>';
 }
 
 
+
 //! 7.
-displayNewList.innerHTML = fullName + newFullName;
+displayNewList.innerHTML = newFullName;
